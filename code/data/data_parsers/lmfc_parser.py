dfa3faa95285dd450d397d2f363ed93e7b855e9b
clinton0313 <clinton0313@gmail.com>
2021-11-03 11:37:03 +0100

    changed .env to os.chdir

--- code/data/data_parsers/lmfc_parser.py
+++ code/data/data_parsers/lmfc_parser.py
@@ -1,12 +1,16 @@
+#%%
 import os
 import pandas as pd
 from datetime import datetime
+from dotenv import load_dotenv
 
+load_dotenv('../../.env')
+
+os.chdir(os.getenv("PATH"))
 def dateparser(dates):
-    '''Parses dates trying all formats.'''
     return datetime.strptime(dates, "%Y-%m-%d")
-    # parsed_dates = [datetime.strptime(date_string, "%Y-%m-%d") for date_string in dates]
-    # return parsed_dates
 
-os.chdir("/home/clinton/Documents/statistical_inference/stat_proj/")
-data = pd.read_table("SOCC_CA_LACounty_data.txt", sep="\t")
+path = "code/data"
+filename = "AICC_data.txt"
+file = os.path.join(path, filename)
+data = pd.read_table(file, sep="\t", date_parser = dateparser, parse_dates=[4], skipinitialspace=True, usecols=list(range(7)))
