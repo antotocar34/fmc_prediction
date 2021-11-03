b9955cb58891a6be94f4f9bd990da5d9648ee7ee
clinton0313 <clinton0313@gmail.com>
2021-11-03 16:03:52 +0100

    Basic data exploration of WFAS data.

--- code/data/data_parsers/lmfc_parser.py
+++ code/data/data_parsers/lmfc_parser.py
@@ -2,15 +2,35 @@
 import os
 import pandas as pd
 from datetime import datetime
-from dotenv import load_dotenv
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+os.chdir("../../..")
 
-load_dotenv('../../.env')
+filelist = os.listdir("code/data/raw_data/wfas")
+filelist.sort()
 
-os.chdir(os.getenv("PATH"))
-def dateparser(dates):
-    return datetime.strptime(dates, "%Y-%m-%d")
+path = "code/data/raw_data"
+filename = "SOCC_data.txt"
 
-path = "code/data"
-filename = "AICC_data.txt"
-file = os.path.join(path, filename)
-data = pd.read_table(file, sep="\t", date_parser = dateparser, parse_dates=[4], skipinitialspace=True, usecols=list(range(7)))
+def read_wfas_table(filename, path):
+    file = os.path.join(path, filename)
+    data = pd.read_table(file, sep="\t", date_parser = lambda date: datetime.strptime(date, "%Y-%m-%d"), parse_dates=[4], skipinitialspace=True, usecols=list(range(7)))
+    return data
+# %%
+#Simple plot to look at observations made per year. 
+socc = read_wfas_table(filelist[7], path)
+socc.groupby(socc["Date"].dt.year).count().plot(kind="bar")
+
+#%%
+#Get an idea of which fuels have good data
+def summarize(cols):
+    '''Output some basic stats on FMC grouped by cols'''
+    summary = socc.groupby(cols).Percent.agg(["max", "min", "mean", "median", "count"])
+    return summary.round(1).sort_values(by="count", ascending = False)
+
+summarize("Fuel")
+summarize(["Site", "Fuel"])
+#%%
+#Need a smart way to title these plots for data exploration
+socc.groupby(["Site", "Fuel"]).plot(x = "Date", y = "Percent", kind="line", subplots=True)
+
+# %%
