7d32eb9aa4341daba3550595d38c14c76b0a770b
clinton0313 <clinton0313@gmail.com>
2021-11-11 23:58:46 +0100

    Cleaned it up and caught a small bug. Retesting

--- code/data/data_parsers/noaa_scraper.py
+++ code/data/data_parsers/noaa_scraper.py
@@ -28,15 +28,6 @@ parameters = {
 # %%
 #Parse and Scrape Functions
 
-# def get_noaa(endpoint, results = True, url = noaa_api, token = token):
-#     '''Returns the list of results from a noaa api request'''
-#     headers = {"token" : token}
-#     r = requests.get(url=url + endpoint, headers = headers)
-#     if results == True:
-#         return r.json()["results"]
-#     else:
-#         return r.json()
-
 def get_noaa(data:str, url = noaa_api, token = token, results = True, override = False, parameters = parameters, **options):
     '''Sets an endpoint to be passed to noaa api.
 
@@ -62,61 +53,28 @@ def get_noaa(data:str, url = noaa_api, token = token, results = True, override =
     endpoint = f"{data}?{'&'.join(optional_params)}"
     headers = {"token" : token}
     r = requests.get(url=url + endpoint, headers = headers)
-    time.sleep(0.5)
+    time.sleep(0.25)
     if results == True:
         return r.json()["results"]
     else:
         return r.json()
+#Sample usage: Shows location data via state. California is FIPS:06, Arizona is FIPS:04, nevada is FIPS:32
+#get_noaa("locations", locationcategoryid="ST", limit = 1000)
+
+def simple_query(index, datatype, df = socc):
+    '''Simple querying for testing purposes. Returns data with index and datatype of df.'''
+    d = str(df.loc[index, "Date"].date())
+    coord = (df.loc[index, "Latitude"], df.loc[index, "Longitude"])
+    stations = pd.DataFrame.from_records(get_noaa("stations", datasetid="GHCND", startdate = d, enddate=d, datatypeid=datatype, limit = 1000))
+    station_id = get_stationid(coord, stations)
+    data = get_noaa("data", datasetid="GHCND", datatypeid=datatype, startdate=d, enddate=d, stationid=station_id)
+    return data
 
 def get_stationid(site_coord, station_df):
     '''Given site_coord as a (lat, long) tuple and a station df outputted by NOAA API, 
     return a Station ID.'''
     distance = [geodist(site_coord, (station_df.loc[i, "latitude"], station_df.loc[i, "longitude"])).km for i in station_df.index]
     return station_df.loc[distance.index(min(distance)), "id"]
-
-# def station_dict(wfas_df, stateid, datasetid):
-#     '''Given a clean dataframe from WFAS with Lat, Long columns, and a stateid for the NOAA API's locationid (ST), 
-#     return a dictionary mapping site names from WFAS to closest station's ID'''
-#     stations = pd.DataFrame.from_records(get_noaa("stations", datasetid=datasetid, locationid=stateid, limit = 1000))
-#     grouped_sites = wfas_df.groupby("Site").mean()
-#     station_dict = {}
-#     for site in tqdm(grouped_sites.index):
-#         site_coord = (grouped_sites.loc[site, "Latitude"], grouped_sites.loc[site, "Longitude"])
-#         station_dict[site] = get_stationid(site_coord, stations)
-#     return station_dict
-#%%
-#Shows location data via state. California is FIPS:06, Arizona is FIPS:04, nevada is FIPS:32
-#get_noaa("locations", locationcategoryid="ST", limit = 1000)
-#%%
-#Load cleaned WFAS data, match to NOAA weather station IDs, save dictionary.
-
-with open("code/data/clean_data/wfas/SOCC_cleaned.pkl", "rb") as infile:
-    socc = pickle.load(infile)
-
-'''Next steps:
-1) Function to get cleaned NOAA weather and/or rainfail datasets based on date and stationID
-2) Apply this function to SOCC cleaned data to form a DF of weather data that can be matched to WFAS DF'''
-# %%
-#List available datatypes for our given date range using GHCND dataset. Might have to do the same for PRECIP_15 dataset
-def get_available_types(noaa_dataset, maxdate, mindate, locationid):
-    '''Get all available datatypes for given noaa dataset and date range.'''
-    all_types = pd.DataFrame.from_records(get_noaa("datatypes", datasetid=noaa_dataset, locationid=locationid, limit=1000))
-    all_types["mindate"] = pd.to_datetime(all_types["mindate"], infer_datetime_format=True)
-    all_types["maxdate"] = pd.to_datetime(all_types["maxdate"], infer_datetime_format=True)
-    all_types = all_types[all_types["mindate"] <= mindate][all_types["maxdate"] >= maxdate]
-    return all_types
-
-GHCND_types = get_available_types("GHCND", maxdate = max(socc["Date"]), mindate=min(socc["Date"]), locationid="FIPS:06")
-# GSOM_types = get_available_types("GSOM", maxdate = max(socc["Date"]) - timedelta(days=62), mindate=min(socc["Date"]), locationid="FIPS:06")
-
-'''Data types of interest:
-GHCND: AWND, DAPR, MDPR, PRCP, SNOW, TAVG, TMAX, TMIN, WSFG, WDFG, WT01, WT04, WT08
-GSOM: TAVG, TMAX, TMIN, TSUN, CLDD, DP01, DP10, DT00, DT32, DX32, DX70, DX90, , EVAP, 
-MN01, MN02, MN03, MX01, MX02, MX03, PRCP'''
-
-
-# %%
-stateids = ["FIPS:06", "FIPS:04", "FIPS:32"] #State ID's for California, Arizona, Nevada
 
 def bin_dates(startdate, enddate):
     '''Bins dates into a list of date ranges no greater than 998 days different'''
@@ -125,13 +83,12 @@ def bin_dates(startdate, enddate):
     dateranges = []
     for i in range(rng//364):
         start = str((startdate + timedelta(days=i*364)).date())
-        stop = str((startdate + timedelta(days=(i+1)*364)).date())
+        stop = str((startdate + timedelta(days=(i+1)*364+1)).date())
         dateranges.append((start, stop))
     dateranges.append((str((startdate + timedelta(days=(rng//364) * 364)).date()), str(enddate.date())))
     return dateranges
 
-
-def get_data(site, datatype, locations = stateids, dataset="GHCND", df = socc, max_iters = 10):
+def get_data(site, datatype, df = socc, dataset="GHCND", max_iters = 10):
     '''Given a site and a datatype, gets the daily datatype reports from closest station in one year intervals. Iterates through to find a station 
     that does not throw an error.'''
     results = []
@@ -140,7 +97,7 @@ def get_data(site, datatype, locations = stateids, dataset="GHCND", df = socc, m
     maxdate = df.groupby("Site").max().loc[site, "Date"] + timedelta(days=1)
     mindate = df.groupby("Site").min().loc[site, "Date"] - timedelta(days=1)
     dateranges = bin_dates(mindate, maxdate)
-    for daterange in tqdm(dateranges, desc=f"Getting data for {site}"):
+    for daterange in tqdm(dateranges, desc=f"Getting {datatype} for {site}"):
         startdate, enddate = daterange
         stations = pd.DataFrame.from_records(get_noaa("stations", datasetid=dataset, startdate = startdate, enddate=enddate, datatypeid=datatype, limit = 1000))
         for i in range(max_iters):
@@ -159,17 +116,6 @@ def get_data(site, datatype, locations = stateids, dataset="GHCND", df = socc, m
                     break
     return results
 
-def simple_query(index, datatype, df = socc):
-    '''Simple querying for testing purposes. Returns data with index and datatype of df.'''
-    d = str(df.loc[index, "Date"].date())
-    coord = (df.loc[index, "Latitude"], df.loc[index, "Longitude"])
-    stations = pd.DataFrame.from_records(get_noaa("stations", datasetid="GHCND", startdate = d, enddate=d, datatypeid=datatype, limit = 1000))
-    station_id = get_stationid(coord, stations)
-    data = get_noaa("data", datasetid="GHCND", datatypeid=datatype, startdate=d, enddate=d, stationid=station_id)
-    return data
-
-
-# %%
 def parse_noaa_query(noaa_query, site, datatype):
     '''Parses the results from NOAA query and returns a dataframe with columns Site, date, {datatype}
     and a list of stations used. '''
@@ -190,8 +136,9 @@ def get_datatype_sitesloop(datatype, data = socc, max_iters = 10):
         noaa_query = get_data(site, datatype, df = socc, max_iters = max_iters)
         df, stations = parse_noaa_query(noaa_query, site, datatype)
         result.append(df, ignore_index=True)
+        result.drop_duplicates(inplace=True, ignore_index=True)
         meta[site] = {datatype: stations}
-    return df, meta
+    return result, meta
 
 def get_all_datatypes(datatypes, data = socc, max_iters = 10):
     '''Given a list of datatypes, iteratively gets all site data for each data type and merges to a copy of the original dataframe, saving to pickle after each datatype.'''
@@ -199,6 +146,7 @@ def get_all_datatypes(datatypes, data = socc, max_iters = 10):
     result = data.copy()
     for datatype in tqdm(datatypes, desc=f"Total Progress for {datatypes}"):
         df, meta = get_datatype_sitesloop(datatype, data=data, max_iters=max_iters)
+
         result.merge(df, how="left", on=["Site", "Date"])
         for site, data_dict in meta.items():
             if site not in metadata.keys():
@@ -210,9 +158,33 @@ def get_all_datatypes(datatypes, data = socc, max_iters = 10):
         with open("code/data/interim_data/socc_metadata.pkl", "wb") as outfile:
             pickle.dump(metadata, outfile)
         print(f"{datatype} saved.")
-
 # %%
-#Tests
+#List available datatypes for a given datarange for a given dataset.
+def get_available_types(noaa_dataset, maxdate, mindate, locationid):
+    '''Get all available datatypes for given noaa dataset and date range.'''
+    all_types = pd.DataFrame.from_records(get_noaa("datatypes", datasetid=noaa_dataset, locationid=locationid, limit=1000))
+    all_types["mindate"] = pd.to_datetime(all_types["mindate"], infer_datetime_format=True)
+    all_types["maxdate"] = pd.to_datetime(all_types["maxdate"], infer_datetime_format=True)
+    all_types = all_types[all_types["mindate"] <= mindate][all_types["maxdate"] >= maxdate]
+    return all_types
+
+#GHCND_types = get_available_types("GHCND", maxdate = max(socc["Date"]), mindate=min(socc["Date"]), locationid="FIPS:06")
+# GSOM_types = get_available_types("GSOM", maxdate = max(socc["Date"]) - timedelta(days=62), mindate=min(socc["Date"]), locationid="FIPS:06")
+
+'''Data types of interest:
+GHCND: AWND, DAPR, MDPR, PRCP, SNOW, TAVG, TMAX, TMIN, WSFG, WDFG, WT01, WT04, WT08
+GSOM: TAVG, TMAX, TMIN, TSUN, CLDD, DP01, DP10, DT00, DT32, DX32, DX70, DX90, , EVAP, 
+MN01, MN02, MN03, MX01, MX02, MX03, PRCP'''
+#%%
+#Use a partial wrapper to amend defaults up there for data.
+
+#%%
+#Load data
+stateids = ["FIPS:06", "FIPS:04", "FIPS:32"] #State ID's for California, Arizona, Nevada
+with open("code/data/clean_data/wfas/SOCC_cleaned.pkl", "rb") as infile:
+    socc = pickle.load(infile)
+# %%
+#Run Scraper
 start = time.time()
 sites = socc["Site"].unique()
 GHCND_types = ["AWND", "DAPR", "MDPR", "PRCP", "SNOW", "TAVG", "TMAX", "TMIN", "WSFG", "WDFG", "WT01", "WT04", "WT08"]
